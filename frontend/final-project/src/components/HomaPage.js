a312662e5dbfc80fb4f1291acf0eb163676e0fec
Sold-T <thomas.soldwedel@gmail.com>
2023-01-12 16:24:02 +0100

    connection between frontend and backend established.

--- frontend/final-project/src/components/HomaPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HomaPage() {
-  return (
-    <div>HomaPage</div>
-  )
-}
--- frontend/final-project/src/context/Container.js
+++ frontend/final-project/src/context/Container.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import MyContext from "./MyContext.js";
+import { useNavigate } from "react-router-dom";
 
 export default function Container(props) {
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch("/users/checkusertoken", {
+        method: "GET",
+        headers: { token: token },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.success) {
+            setUser(result.data);
+          } else {
+            navigate("/login");
+          }
+        });
+    }
+  }, []);
+
   return (
-    <div>
-    {props.children} 
-  </div>
-  )
+    <MyContext.Provider value={{ user, setUser }}>
+      {props.children}
+    </MyContext.Provider>
+  );
 }
