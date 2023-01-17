878c819ea640750b3b5a9b9da4ab642d93bfbd45
Sold-T <thomas.soldwedel@gmail.com>
2023-01-17 14:35:44 +0100

    structured UsersLandingPage.js and cleaned up HomePage.js again. Routes for Profile, Settings and Notes are in place

--- frontend/final-project/src/App.js
+++ frontend/final-project/src/App.js
@@ -4,18 +4,32 @@ import UsersLandingPage from "./components/UsersLandingPage";
 import Register from "./components/Register.js";
 import Login from "./components/Login.js";
 import HomePage from "./components/HomePage";
+import ProfilePage from "./components/ProfilePage";
+import Settings from "./components/Settings";
+import Notes from "./components/Notes";
 
 function App() {
-
   return (
-
     <div className="App">
+
       <Routes>
-        <Route path="/" element={<HomePage></HomePage>} >
+        <Route path="/" element={<HomePage></HomePage>}>
           <Route path="/login" element={<Login></Login>} />
           <Route path="/users" element={<Register></Register>} />
         </Route>
-        <Route path="/home" element={<UsersLandingPage></UsersLandingPage>} />
+
+        <Route path="/home" element={<UsersLandingPage></UsersLandingPage>}>
+          <Route path="/home/profile" element={<ProfilePage></ProfilePage>}></Route>
+          <Route path="/home/settings" element={<Settings></Settings>}></Route>
+
+          <Route path="/home/notes/all" element={<Notes></Notes>}></Route>
+          <Route path="/home/notes/personal" element={<Notes></Notes>}></Route>
+          <Route path="/home/notes/business" element={<Notes></Notes>}></Route>
+          <Route path="/home/notes/education" element={<Notes></Notes>}></Route>
+          
+        </Route>
+
+
       </Routes>
     </div>
   );
--- frontend/final-project/src/components/UsersLandingPage.js
+++ frontend/final-project/src/components/UsersLandingPage.js
@@ -1,8 +1,112 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import MyContext from "../context/MyContext";
+import testImage from "./testImage.jpg";
+import "./UsersLandingPage.css";
 
 export default function UsersLandingPage() {
+  const { user, setUser } = useContext(MyContext);
+  const navigate = useNavigate();
 
-  //it will return our users landing page when users log-in
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/login");
+  };
 
-  return <div>Users Landing Page</div>;
+  return (
+    <div>
+      {user ? (
+        <>
+          <div className="header">
+            <NavLink to="/home"> Brand Name / Logo </NavLink> <br></br>
+            <button onClick={logout}>Logout </button>
+          </div>
+
+          <div className="navigations">
+            <div className="side-navigation">
+              <div className="profile-settings">
+                <img
+                  alt=""
+                  src={testImage}
+                  style={{
+                    width: "133px",
+                    marginBottom: "2rem",
+                    borderRadius: "50%",
+                  }}
+                ></img>
+                <NavLink to="/home/profile"> Profile </NavLink>
+                <br></br>
+                <NavLink to="/home/settings"> Settings </NavLink>
+                <br></br>
+              </div>
+              <div className="note-categories">
+                <h3>Navigation</h3>
+                <NavLink className="category" to="/home/notes/all">
+                  {" "}
+                  All{" "}
+                </NavLink>
+                <br></br>
+                <NavLink className="category" to="/home/notes/personal">
+                  {" "}
+                  Personal{" "}
+                </NavLink>
+                <br></br>
+                <NavLink className="category" to="/home/notes/business">
+                  {" "}
+                  Business{" "}
+                </NavLink>
+                <br></br>
+                <NavLink className="category" to="/home/notes/education">
+                  {" "}
+                  Education{" "}
+                </NavLink>
+                <br></br>
+                <NavLink className="category" to="/home/notes/education">
+                  {" "}
+                  <button>Add Section</button>{" "}
+                </NavLink>
+                <br></br>
+              </div>
+            </div>
+                  
+            <div className="yellow">
+              <h1>Outlet - Placeholder for Features in the middle</h1>
+              <Outlet></Outlet>
+              
+            </div>
+
+            <div className="red">
+              <div className="purple">
+                <h1>Calendar</h1>
+              </div>
+
+              <div className="green">
+                <h1>Spotify</h1>
+              </div>
+
+              <div className="blue">
+                <h1>Other</h1>
+              </div>
+            </div>
+          </div>
+          
+          <footer>
+            <h1>Footer</h1>
+          </footer>
+        </>
+      ) : (
+        <>
+          <nav>
+            <NavLink to="/login"> Login </NavLink>
+            <br></br>
+            <NavLink to="/users"> Register </NavLink>
+            <br></br>
+          </nav>
+        </>
+      )}
+
+    </div>
+  );
 }
