533d0cc99b8e97acacc3910b8506b3d4f4b0665f
heyBr0 <benert@live.de>
2023-01-25 14:21:03 +0100

    added notes page and test calender

--- frontend/final-project/src/App.js
+++ frontend/final-project/src/App.js
@@ -6,7 +6,7 @@ import Login from "./components/Login.js";
 import HomePage from "./components/HomePage";
 import Profile from "./components/ProfilePage";
 import Settings from "./components/Settings";
-import Notes from "./components/Kanban";
+import Notes from "./components/Notes";
 import EditProfileUser from "./components/EditProfilePage.js";
 import Kanban from "./components/Kanban";
 
@@ -19,7 +19,9 @@ function App() {
         <Route path="/" element={<HomePage></HomePage>}>
           <Route path="/login" element={<Login></Login>} />
           <Route path="/users" element={<Register></Register>} />
-        </Route>
+         </Route>
+
+         <Route path="/notes" element={<Notes></Notes>}></Route>
 
         <Route path="/home" element={<UsersLandingPage></UsersLandingPage>}>
           <Route path="/home/profile" element={<Profile></Profile>}></Route>
@@ -28,9 +30,9 @@ function App() {
           <Route path="/home/all" element={<Kanban></Kanban>}></Route>
           <Route path="/home/kanban" element={<Kanban></Kanban>}></Route>
           <Route path="/home/" element={<Kanban></Kanban>}></Route>
-          <Route path="/home/" element={<Kanban></Kanban>}></Route>
+          <Route path="/home/" element={<Kanban></Kanban>}></Route>        
         </Route>
-
+    
       </Routes>
     </div>
   );
--- frontend/final-project/src/components/Notes.js
+++ frontend/final-project/src/components/Notes.js
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import "../styles/NotesStyles.css";
 import { NavLink, Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import MyContext from "../context/MyContext";
-import testImage from "./testImage.jpg";
-import "./UsersLandingPage.css";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
+export default function Notes() {
+  // calender
+  const [value, onChange] = useState(new Date());
 
-export default function UsersLandingPage() {
   const { user, setUser } = useContext(MyContext);
   const navigate = useNavigate();
 
@@ -15,7 +18,6 @@ export default function UsersLandingPage() {
     setUser(null);
     navigate("/login");
   };
-
   return (
     <div>
       {user ? (
@@ -25,18 +27,11 @@ export default function UsersLandingPage() {
             <button onClick={logout}>Logout </button>
           </div>
 
+          {/*  */}
+
           <div className="navigations">
             <div className="side-navigation">
               <div className="profile-settings">
-                <img
-                  alt=""
-                  src={testImage}
-                  style={{
-                    width: "133px",
-                    marginBottom: "2rem",
-                    borderRadius: "50%",
-                  }}
-                ></img>
                 <NavLink to="/home/profile"> Profile </NavLink>
                 <br></br>
                 <NavLink to="/home/settings"> Settings </NavLink>
@@ -73,14 +68,15 @@ export default function UsersLandingPage() {
             </div>
 
             <div className="yellow">
-              <h1>Outlet - Placeholder for Features in the middle</h1>
+              <h1 className="yellowH1">Notes</h1>
               <Outlet></Outlet>
             </div>
 
             <div className="red">
               <div className="purple">
-                <h1>Feature</h1>
-                <p>e.g. Calendar</p>
+                <div >
+                  <Calendar onChange={onChange} value={value} />
+                </div>
               </div>
 
               <div className="green">
@@ -97,18 +93,17 @@ export default function UsersLandingPage() {
           <footer>
             <h1>Footer</h1>
           </footer>
+
+          {/*  */}
         </>
       ) : (
-        <>
-          <nav>
-            <NavLink to="/login"> Login </NavLink>
-            <br></br>
-            <NavLink to="/users"> Register </NavLink>
-            <br></br>
-          </nav>
-        </>
+        <nav>
+          <NavLink to="/login"> Login </NavLink>
+          <br></br>
+          <NavLink to="/users"> Register </NavLink>
+          <br></br>
+        </nav>
       )}
     </div>
   );
-
 }
